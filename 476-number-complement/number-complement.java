84d96cc3513ddf75da5d723bd45108ba1045afab
Digvijay Devkar <152246733+digvijaydevkar@users.noreply.github.com>
2026-09-03 16:11:49 +0530

    Time: 1 ms (18.47%) | Memory: 41.6 MB (99.08%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int num) {
+        String s = Integer.toBinaryString(num);
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray())
+        {
+            if(c == '0') sb.append('1');
+            else sb.append('0');
+        }
+        return Integer.parseInt(sb.toString(),2);
+    }
+}
